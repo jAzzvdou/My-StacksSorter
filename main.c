260736b0fd0a9f0e29b6139c1067f952fdab322d
jazevedo <joaoazevedo592@gmail.com>
2024-01-30 19:08:01 -0300

    Main

--- main.c
+++ main.c
@@ -6,11 +6,19 @@
 /*   By: jazevedo <jazevedo@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 15:09:31 by jazevedo          #+#    #+#             */
-/*   Updated: 2024/01/16 17:23:09 by jazevedo         ###   ########.fr       */
+/*   Updated: 2024/01/30 16:49:08 by jazevedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+		return (write(2, "\n", 1));
+	if (argc == 2)
+		argv = ft_split(argv[1], ' ');
+
 
 int	main(int argc, char **argv)
 {
@@ -26,3 +34,15 @@ int	main(int argc, char **argv)
 	}
 	return (0);
 }
+
+//SE FOR CARACTERES NÃO NUMÉRICOS: Error\n
+//SE FOR ACIMA DO MAXINT: Error\n
+//SE FOR ABAIXO DO MININT: Error\n
+//SE FOREM NÚMEROS DUPLICADOS: Error\n
+//SE FOR SÓ ./push_swap: \n
+//SE OS NÚMEROS JÁ ESTIVEREM ORDENADOS: \n
+
+//PARA 3 ELEMENTOS: MÁXIMO DE 3 MOVIMENTOS -> 6 WAYS (UM BANDO DE IF)
+//PARA 5 ELEMENTOS: MÁXIMO DE 12 MOVIMENTOS -> 120 WAYS (ALGORITMO BÁSICO)
+//PARA 100 ELEMENTOS: MÁXIMO DE 700 MOVIMENTOS -> 100! WAYS (ALGORITMO AVANÇADO)
+//PARA 500 ELEMENTOS: MÁXIMO DE 5500 MOVIMENTOS -> 500! WAYS (ALGORITMO AVANÇADO)

f215814421c1fa030f7961ba00c20173de75a2eb
jAzzvdou <joaoazevedo592@gmail.com>
2024-02-01 12:04:48 -0300

    New_organization_function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jazevedo <jazevedo@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 15:09:31 by jazevedo          #+#    #+#             */
-/*   Updated: 2024/02/01 11:31:04 by jazevedo         ###   ########.fr       */
+/*   Updated: 2024/02/01 12:04:05 by jazevedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,21 +32,29 @@ void	first_algorithm(t_pushswap *pushswap)
 	}
 }
 
+int	invalid_arguments(int argc, char **argv)
+{
+        if (argc == 1 || (argc == 2 && !argv[1][0]))
+                return (write(2, "\n", 1));
+        if (argc == 2)
+                argv = ft_split(argv[1], ' ');
+        if (!verify_argv(argc, argv))
+                return (write(2, "Error\n", 6));
+        if (!is_int(argv))
+                return (write(2, "Error\n", 6));
+        if (is_duplicated(argv))
+                return (write(2, "Error\n", 6));
+        if (!in_stack(&pushswap, argc, argv))
+                return (1);
+	return (0);
+{
+	
+
 int	main(int argc, char **argv)
 {
 	t_pushswap	pushswap = {0};
 
-	if (argc == 1 || (argc == 2 && !argv[1][0]))
-		return (write(2, "\n", 1));
-	if (argc == 2)
-		argv = ft_split(argv[1], ' ');
-	if (!verify_argv(argc, argv))
-		return (write(2, "Error\n", 6));
-	if (!is_int(argv))
-		return (write(2, "Error\n", 6));
-	if (is_duplicated(argv))
-		return (write(2, "Error\n", 6));
-	if (!in_stack(&pushswap, argc, argv))
+	if (invalid_arguments(argc, argv))
 		return (1);
 	while (!is_sorted(pushswap.a) || pushswap.b)
 	{
@@ -57,19 +65,8 @@ int	main(int argc, char **argv)
 			break ;
 		}
 		else if (stack_size(pushswap.a) == 3)
-			first_algorithm(&pushswap);
-		// if stack have 5 elements = second algorithm
-		// the second algorithm max moves is 12
-		/*
-		else if (stack_size(pushswap.a) == 4
-			|| stack_size(pushswap.a) == 5)
-			second_algorithm(&pushswap);
-		// if stack have more than 5 elements = third algorithm
-		// the third algorithm max moves is 700
-		else if (stack_size(pushswap.a) > 5)
-			third_algorithm(&pushswap);
-		*/
-	}	
+			first_algorithm(&pushswap);	
+	}
 	return (0);
 }
 
